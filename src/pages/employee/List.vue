8c7e7405d39f798636aa3b0016ae47638d3e9055
zhaoyikang199942 <1026210340@qq.com>
2020-01-04 09:21:15 +0800

    master

---
+++ src/pages/employee/List.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        员工 管理
+    </div>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -54,99 +54,55 @@ export const constantRoutes = [
       meta: { title: 'Dashboard', icon: 'dashboard' }
     }]
   },
-
   {
-    path: '/example',
-    component: Layout,
-    redirect: '/example/table',
-    name: 'Example',
-    meta: { title: 'Example', icon: 'example' },
-    children: [
-      {
-        path: 'table',
-        name: 'Table',
-        component: () => import('@/views/table/index'),
-        meta: { title: 'Table', icon: 'table' }
-      },
-      {
-        path: 'tree',
-        name: 'Tree',
-        component: () => import('@/views/tree/index'),
-        meta: { title: 'Tree', icon: 'tree' }
-      }
-    ]
-  },
-
-  {
-    path: '/form',
+    path: '/customer',
     component: Layout,
     children: [
       {
-        path: 'index',
-        name: 'Form',
-        component: () => import('@/views/form/index'),
-        meta: { title: 'Form', icon: 'form' }
+        path: 'list',
+        name: 'customerList',
+        component: () => import('@/pages/customer/List'),
+        meta: { title: '顾客管理', icon: 'user' }
       }
     ]
   },
-
   {
-    path: '/nested',
+    path: '/employee',
     component: Layout,
-    redirect: '/nested/menu1',
-    name: 'Nested',
-    meta: {
-      title: 'Nested',
-      icon: 'nested'
-    },
     children: [
       {
-        path: 'menu1',
-        component: () => import('@/views/nested/menu1/index'), // Parent router-view
-        name: 'Menu1',
-        meta: { title: 'Menu1' },
-        children: [
-          {
-            path: 'menu1-1',
-            component: () => import('@/views/nested/menu1/menu1-1'),
-            name: 'Menu1-1',
-            meta: { title: 'Menu1-1' }
-          },
-          {
-            path: 'menu1-2',
-            component: () => import('@/views/nested/menu1/menu1-2'),
-            name: 'Menu1-2',
-            meta: { title: 'Menu1-2' },
-            children: [
-              {
-                path: 'menu1-2-1',
-                component: () => import('@/views/nested/menu1/menu1-2/menu1-2-1'),
-                name: 'Menu1-2-1',
-                meta: { title: 'Menu1-2-1' }
-              },
-              {
-                path: 'menu1-2-2',
-                component: () => import('@/views/nested/menu1/menu1-2/menu1-2-2'),
-                name: 'Menu1-2-2',
-                meta: { title: 'Menu1-2-2' }
-              }
-            ]
-          },
-          {
-            path: 'menu1-3',
-            component: () => import('@/views/nested/menu1/menu1-3'),
-            name: 'Menu1-3',
-            meta: { title: 'Menu1-3' }
-          }
-        ]
-      },
-      {
-        path: 'menu2',
-        component: () => import('@/views/nested/menu2/index'),
-        meta: { title: 'menu2' }
+        path: 'list',
+        name: 'employeeList',
+        component: () => import('@/pages/employee/List'),
+        meta: { title: '员工管理', icon: 'user' }
       }
     ]
   },
+  {
+    path: '/column',
+    component: Layout,
+    children: [
+      {
+        path: 'list',
+        name: 'columnList',
+        component: () => import('@/pages/column/List'),
+        meta: { title: '栏目管理', icon: 'user' }
+      }
+    ]
+  },
+  {
+    path: '/product',
+    component: Layout,
+    children: [
+      {
+        path: 'list',
+        name: 'productList',
+        component: () => import('@/pages/product/List'),
+        meta: { title: '产品管理', icon: 'user' }
+      }
+    ]
+  },
+  
 
   {
     path: 'external-link',
